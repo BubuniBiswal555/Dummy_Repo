bb21c2e6c9fdbc01140d0848819768bd8dae36e3
AMULYA <AMULYA@DESKTOP-GE4GJ1U>
2025-07-02 13:49:16 +0530

    electronic added

---
+++ Automation_Framework_1/src/test/java/electronics/ElectronicTest.java
@@ -0,0 +1,20 @@
+package electronics;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+
+import objectrepository.HomePage;
+
+public class ElectronicTest {
+@Test
+	
+	public void clickOnBooks() {
+		
+		
+		Reporter.log("bhuvn",true);		
+	}
+
+}
